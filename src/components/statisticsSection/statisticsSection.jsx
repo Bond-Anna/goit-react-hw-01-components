1d6a62f5b2fa850db3cfd71e26ba987adeb9b723
Anna Bondarenko <anna93bondarenko@gmail.com>
2021-09-16 16:12:35 +0300

    fixing

--- src/components/statisticsSection/statisticsSection.jsx
+++ src/components/statisticsSection/statisticsSection.jsx
@@ -3,7 +3,7 @@ import css from './statisticsSection.module.css';
 export const Statistics = ({ stats, title }) => {
   return (
     <section className={css.statistics}>
-      {{ title } && <h2 className={css.title}>{title}</h2>}
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {stats.map(stat => (
           <li className={css.item} key={stat.id}>
